9b98582256a6574f004a900b42de74eaf74a05c2
Андрей Лаптев <laazp@ukr.net>
2020-06-01 14:33:01 +0300

    Where and Select implemented

---
+++ LinqReimplementation/LinqExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqReimplementation
+{
+    public static class LinqExtension
+    {
+        public static IEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (predicate == null)
+            {
+                throw new ArgumentNullException(nameof(predicate));
+            }
+
+            foreach (TSource item in source)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            foreach (TSource item in source)
+            {
+                yield return selector(item);
+            }
+        }
+    }
+}
---
+++ LinqReimplementation/Program.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using System;
+using System.Collections.Generic;
+
+namespace LinqReimplementation
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faker<Person> generatorPerson = getGeneratorPerson();
+            List<Person> persons = generatorPerson.Generate(100);
+
+            IEnumerable<int> editedPersons = persons.Select(x=>x.Age);
+
+            foreach(int item in editedPersons)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        private static Faker<Person> getGeneratorPerson()
+        {
+            return new Faker<Person>("ru")
+                .RuleFor(x => x.FullName, f => f.Name.FullName())
+                .RuleFor(x => x.Age, f => f.Random.Byte(16, 100))
+                .RuleFor(x => x.Phone, f => f.Phone.PhoneNumber());
+        }
+
+    }
+}
